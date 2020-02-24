44d37a8c07f394bd0dd0e9f34ed01639dec6b9c9
Nikki Zhao <56944886+nikkizhao1202@users.noreply.github.com>
2020-02-24 11:03:33 -0500

    tweets cnn model and result

---
+++ CNN_MODEL/sentiment_dataset.py
@@ -0,0 +1,95 @@
+
+import os
+import json
+import math
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+import boto3
+
+# from tf.data import Dataset
+
+
+
+def train_input_fn(training_dir, config):
+    return _input_fn(training_dir, config, "train")
+
+def validation_input_fn(training_dir, config):
+    return _input_fn(training_dir, config, "validation")
+
+def eval_input_fn(training_dir, config):
+    return _input_fn(training_dir, config, "eval")
+
+def serving_input_fn(_, config):
+    # Here it concerns the inference case where we just need a placeholder to store
+    # the incoming images ...
+    tensor = tf.placeholder(dtype=tf.float32, shape=[1, config["embeddings_vector_size"]])
+    inputs = {config["input_tensor_name"]: tensor}
+    return tf.estimator.export.ServingInputReceiver(inputs, inputs)
+
+def _load_json_file(json_path, config):
+
+    features = []
+    labels = []
+    
+    s3 = boto3.resource('s3') 
+    obj = s3.Object('twitter-text', json_path)
+    file = obj.get()['Body'].read().decode()
+    
+    for line in file.split('\n'):
+
+        entry = json.loads(line)
+
+        if len(entry["features"]) != config["padding_size"]:
+            raise ValueError(
+                "The size of the features of the entry with twitterid {} was not expected".format(
+                    entry["twitterid"]))
+
+        labels.append(float(entry["sentiment"]) / 4)
+        features.append(entry["features"])
+
+    return features, labels
+
+def _input_fn(directory, config, mode):
+
+    print("Fetching {} data...".format(mode))
+    
+    client = boto3.client('s3')
+
+    all_files = client.list_objects_v2(Bucket='twitter-text', Prefix= directory)['Contents']
+    
+    all_features = []
+    all_labels = []
+    
+    for i in range(1,len(all_files)):
+        features, labels = _load_json_file(all_files[i]['Key'], config)
+        all_features += features
+        all_labels += labels
+        # print(len(all_features))
+
+    num_data_points = len(all_features)
+    num_batches = math.ceil(len(all_features) / config["batch_size"])
+
+    dataset = tf.data.Dataset.from_tensor_slices((all_features, all_labels))
+
+    if mode == "train":
+
+        dataset = tf.data.Dataset.from_tensor_slices((all_features, all_labels))
+        dataset = dataset.batch(config["batch_size"]).shuffle(10000, seed=12345).repeat(
+            config["num_epoch"])
+        num_batches = math.ceil(len(all_features) / config["batch_size"])
+
+    if mode in ("validation", "eval"):
+
+        dataset = dataset.batch(config["batch_size"]).repeat(config["num_epoch"])
+        num_batches = math.ceil(len(all_features) / config["batch_size"])
+
+    iterator = dataset.make_one_shot_iterator()
+    dataset_features, dataset_labels = iterator.get_next()
+
+    return [{config["input_tensor_name"]: dataset_features}, dataset_labels,
+            {"num_data_point": num_data_points, "num_batches": num_batches}]
+
+
+
---
+++ CNN_MODEL/sentiment_model_cnn.py
@@ -0,0 +1,92 @@
+"""
+Model definition for CNN sentiment training
+
+
+"""
+
+import os
+import tensorflow as tf
+import numpy as np
+import boto3
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Convolution1D, GlobalMaxPooling1D
+from tensorflow.keras.optimizers import Adam
+
+
+
+
+def keras_model_fn(_, config):
+    """
+    Creating a CNN model for sentiment modeling
+
+    """
+
+    '''
+    layer1: embedding layer
+    '''
+    input_length = config["padding_size"]
+    input_dim = config["embeddings_dictionary_size"]
+    output_dim = config["embeddings_vector_size"]
+       
+    client = boto3.client('s3')
+    s3 = boto3.resource('s3') 
+    obj = client.get_object(Bucket='twitter-text', Key='training/data/glove.twitter.27B.25d.txt')
+    contents = obj['Body'].read().decode()
+    glove = contents.split('\n')
+
+    embedding_matrix = np.zeros((input_dim, output_dim))
+
+    for i in range(input_dim):
+        if len(glove[i].split()[1:]) != output_dim :
+            continue
+        embedding_matrix[i] = np.asarray(glove[i].split()[1:], dtype='float32')
+
+
+    embedding_layer = Embedding(input_dim, output_dim, weights=[embedding_matrix],input_length=input_length, trainable=True)
+
+    cnn_model = Sequential() 
+    cnn_model.add(embedding_layer)
+
+
+    """
+    Layer2: Convolution1D layer
+    """
+    cnn_model.add(Convolution1D(
+        filters=100,
+        kernel_size=2,
+        input_shape=(input_length, input_dim),
+        strides=1,
+        padding='valid',
+        activation='relu'
+    ))
+
+    """
+        Layer3: GlobalMaxPool1D layer
+        """
+    cnn_model.add(GlobalMaxPooling1D())
+    """
+    Layer4: Dense layer
+    """
+    cnn_model.add(Dense(units=100, activation='relu'))
+    """
+    Layer5: Dense layer
+    """
+    cnn_model.add(Dense(units=1, activation='sigmoid'))
+    opt = Adam()
+    cnn_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+
+    return cnn_model
+
+def save_model(model, output):
+    """
+    Method to save models in SaveModel format with signature to allow for serving
+    """
+
+    tf.saved_model.save(model, os.path.join(output, "1"))
+
+    print("Model successfully saved at: {}".format(output))
+
